f496c320e6acb81b0bdd023172fa67d5025daddc
bahrr <68436828+bahrr@users.noreply.github.com>
2021-06-07 21:39:37 -0600

    Update and rename rec.ino to directinput.ino

--- rec/directinput.ino
+++ rec/directinput.ino
@@ -1,12 +1,4 @@
-
-/*
-* Arduino Wireless Communication Tutorial
-*       Example 1 - Receiver Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
+//upload this to a Leonardo or other ATMega32U4 Arduino
 
 #include <SPI.h>
 #include <nRF24L01.h>
