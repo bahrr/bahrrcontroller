5865a82924993f36a992c505625b0064f07bc57c
bahrr <68436828+bahrr@users.noreply.github.com>
2021-06-07 21:42:46 -0600

    Update and rename trans/trans.ino to transmitter/transtransmitter.ino

--- transmitter/transtransmitter.ino
+++ transmitter/transtransmitter.ino
@@ -1,11 +1,4 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
+//put this on any Arduino as a transmitter
 
 #include <SPI.h>
 #include <nRF24L01.h>
